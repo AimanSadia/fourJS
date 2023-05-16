bad8499a09020741aa80fe0d2fb8034b9fa1e899
Aiman Sdia <102213880+AimanSadia@users.noreply.github.com>
2023-05-16 08:26:10 +0500

    Add files via upload

---
+++ ass04.js
@@ -0,0 +1,35 @@
+
+/* -----------------------ASSIGNMENT 04 -------------------------------- */
+
+/* ===========QUESTION NO 01 =============*/
+
+/* var a = 2, b = 3, c = 4; */
+
+/* ===========QUESTION NO 02 =============*/
+
+/* Legal Variables */
+
+/* var myAge;
+var $myage;
+var my$age;
+var _myage;
+var my_age;
+var myage123; */
+
+/*Illegal Variaables */
+
+/* var 123myage;
+var @myage;
+var var;
+var my age;
+var %myage;
+var my-age; */
+
+/* ===========QUESTION NO 03 =============*/
+
+/* document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable.</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name.</p>"
+);
+document.write("<p>Variable names are case-sensitive.</p>");
+document.write("<p>Variable names should not be JavaScript keywords.</p>"); */
